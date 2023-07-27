eb1129877c34ea0c1ccadd86a089ead94cb901c9
EMG2311 <germanmontirubio@gmail.com>
2023-07-27 11:07:50 -0300

    feat TaskRepository: Agregar anotation

--- src/main/java/com/task/demo/repository/TaskRepository.java
+++ src/main/java/com/task/demo/repository/TaskRepository.java
@@ -2,6 +2,8 @@ package com.task.demo.repository;
 
 import com.task.demo.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TaskRepository extends JpaRepository<Task,Integer> {
 }
