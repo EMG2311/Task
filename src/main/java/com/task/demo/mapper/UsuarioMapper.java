f80bb0a91371d6a76152f9f2a041596d7dbab57b
EMG2311 <germanmontirubio@gmail.com>
2023-07-27 12:17:30 -0300

    feat UsuarioMapper: se creo clase

---
+++ src/main/java/com/task/demo/mapper/UsuarioMapper.java
@@ -0,0 +1,20 @@
+package com.task.demo.mapper;
+
+import com.task.demo.dto.UsuarioDto;
+import com.task.demo.model.Usuario;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UsuarioMapper {
+
+    public UsuarioDto converToUsuarioDto(Usuario usuario){
+        return new UsuarioDto( usuario.getId(),usuario.getMail(),usuario.getContraseña(),usuario.getEstado());
+    }
+
+    public Usuario converToUsuario(UsuarioDto usuarioDto){
+        return new Usuario(usuarioDto.getId(),usuarioDto.getMail(),usuarioDto.getContraseña(),usuarioDto.getEstado());
+    }
+
+}
