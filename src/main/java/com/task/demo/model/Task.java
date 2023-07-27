029e92617b0a66fea592f9a43cafd39edf72184d
EMG2311 <germanmontirubio@gmail.com>
2023-07-27 10:55:42 -0300

    feat Task: agregar campos

--- src/main/java/com/task/demo/model/Task.java
+++ src/main/java/com/task/demo/model/Task.java
@@ -1,4 +1,24 @@
 package com.task.demo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Task {
+    @Id
+    private Integer id;
+    private String nombre;
+    private String descripcion;
+    private Boolean check;
+    @ManyToOne
+    private Usuario usuario;
+
 }
