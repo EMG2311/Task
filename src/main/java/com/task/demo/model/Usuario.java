a4a767bb889c4ec83962c6cb6360c34e72926de9
EMG2311 <germanmontirubio@gmail.com>
2023-07-27 11:02:43 -0300

    feat Usuario: agregar campos

--- src/main/java/com/task/demo/model/Usuario.java
+++ src/main/java/com/task/demo/model/Usuario.java
@@ -1,6 +1,17 @@
 package com.task.demo.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private String id;
+    @Column(nullable = false,unique = true)
     private String mail;
     private String contraseña;
 }
